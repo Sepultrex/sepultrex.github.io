c76ffbe70826fe8e17c16fe1cc8a5f5f57dd34eb
Sepultrex <sepultrex@gmail.com>
2025-05-07 17:44:58 +0300

    added dialog selections and configured javascript to control them

--- v4/persona.js
+++ v4/persona.js
@@ -7,7 +7,7 @@ function slideImages() {
 
 function kararka() {
     var overlay = document.createElement('div');
-    overlay.id = 'overlay';
+    overlay.id = 'karaaa';
 
     document.body.appendChild(overlay);
 
@@ -16,4 +16,33 @@ function kararka() {
     overlay.onclick = function() {
         overlay.style.display = 'none';
     };
+}
+
+const layers = [
+  document.getElementById('layer1'),
+  document.getElementById('layer2'),
+  document.getElementById('layer3')
+];
+
+let currentIndex = 1; // ortadaki
+layers[currentIndex].classList.add('active');
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'w' || e.key === 'W') {
+    if (currentIndex > 0) {
+      layers[currentIndex].classList.remove('active');
+      currentIndex--;
+      layers[currentIndex].classList.add('active');
+    }
+  } else if (e.key === 's' || e.key === 'S') {
+    if (currentIndex < layers.length - 1) {
+      layers[currentIndex].classList.remove('active');
+      currentIndex++;
+      layers[currentIndex].classList.add('active');
+    }
+  }
+});
+
+function secimmenu() {
+  document.getElementById("imagecontainer").classList.add("activemenu");
 }
